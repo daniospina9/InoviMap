a9eab2aed53d37e2043a1f6eb7d78e861d106fde
daniospina <ingedanielospina9@gmail.com>
2025-10-16 15:32:07 -0500

    feat: some details of loginscreen

--- app/src/main/java/com/example/inovimap/feature/login/LoginScreen.kt
+++ app/src/main/java/com/example/inovimap/feature/login/LoginScreen.kt
@@ -2,6 +2,7 @@ package com.example.inovimap.feature.login
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
@@ -13,10 +14,11 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import androidx.lifecycle.viewmodel.compose.viewModel
 
 @Composable
 fun LoginScreen(
-viewModel: LoginViewModel
+    viewModel: LoginViewModel = viewModel()
 ) {
     val state by viewModel.state.collectAsStateWithLifecycle()
 
@@ -24,6 +26,7 @@ viewModel: LoginViewModel
         Column(
             modifier = Modifier
                 .padding(innerPadding)
+                .fillMaxSize()
                 .padding(24.dp),
             horizontalAlignment = Alignment.CenterHorizontally,
             verticalArrangement = Arrangement.Center
@@ -33,12 +36,22 @@ viewModel: LoginViewModel
             )
             TextField(
                 value = state.user,
+                label = {
+                    Text(
+                        text = "Usuario"
+                    )
+                },
                 onValueChange = { user ->
                     viewModel.setUser(user)
                 }
             )
             TextField(
                 value = state.password,
+                label = {
+                    Text(
+                        text = "Contraseña"
+                    )
+                },
                 onValueChange = { password ->
                     viewModel.setPassword(password)
                 }
@@ -50,6 +63,9 @@ viewModel: LoginViewModel
                     text = "Iniciar Sesión"
                 )
             }
+            Text(
+                text = "Todos los derechos reservados 2025"
+            )
         }
     }
 }
