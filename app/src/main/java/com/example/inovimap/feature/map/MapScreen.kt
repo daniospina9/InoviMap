3f594870fd3659d7efaaa4a4a40fac8e979d4122
daniospina <ingedanielospina9@gmail.com>
2025-10-16 15:43:57 -0500

    feat:added first map screen

---
+++ app/src/main/java/com/example/inovimap/feature/map/MapScreen.kt
@@ -0,0 +1,30 @@
+package com.example.inovimap.feature.map
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun MapScreen(
+
+) {
+    Scaffold { innerPadding ->
+        Column(
+            modifier = Modifier
+                .padding(innerPadding)
+                .fillMaxSize()
+                .padding(24.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(
+                text = "Esta es la pantalla del mapa"
+            )
+        }
+    }
+}
