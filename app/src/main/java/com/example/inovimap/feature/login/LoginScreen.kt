4c20160d8140497d8eae9a03de9cc814fd53b0e4
daniospina <ingedanielospina9@gmail.com>
2025-10-17 07:19:49 -0500

    feat: finished login screen

--- app/src/main/java/com/example/inovimap/feature/login/LoginScreen.kt
+++ app/src/main/java/com/example/inovimap/feature/login/LoginScreen.kt
@@ -1,25 +1,38 @@
 package com.example.inovimap.feature.login
 
 import android.widget.Toast
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.navigation.NavController
 import com.example.inovimap.navigation.map.MapRoute
+import com.example.inovimap.ui.theme.BackgroundColor
+import com.example.inovimap.ui.theme.BackgroundTextFieldColor
+import com.example.inovimap.ui.theme.LoginButtonColor
 
 @Composable
 fun LoginScreen(
@@ -53,47 +66,91 @@ fun LoginScreen(
         Column(
             modifier = Modifier
                 .padding(innerPadding)
+                .background(BackgroundColor)
                 .fillMaxSize()
                 .padding(24.dp),
             horizontalAlignment = Alignment.CenterHorizontally,
             verticalArrangement = Arrangement.Center
         ) {
+            Spacer(modifier = Modifier.weight(0.26f))
             Text(
-                text = "InoviMap"
+                text = "InoviMap",
+                color = Color.White,
+                fontSize = 27.sp,
+                fontWeight = FontWeight.ExtraBold
             )
+            Spacer(modifier = Modifier.weight(0.26f))
             TextField(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(start = 30.dp, end = 30.dp),
                 value = state.user,
                 label = {
                     Text(
-                        text = "Usuario"
+                        text = "User",
+                        color = Color.White
                     )
                 },
                 onValueChange = { user ->
                     viewModel.setUser(user)
-                }
+                },
+                colors = TextFieldDefaults.colors(
+                    focusedContainerColor = BackgroundTextFieldColor,
+                    unfocusedContainerColor = BackgroundTextFieldColor,
+                    disabledContainerColor = BackgroundTextFieldColor,
+                    focusedTextColor = Color.White,
+                    unfocusedTextColor = Color.White,
+                    cursorColor = Color.White,
+                    focusedIndicatorColor = BackgroundTextFieldColor,
+                    unfocusedIndicatorColor = BackgroundTextFieldColor,
+                )
             )
+            Spacer(modifier = Modifier.height(18.dp))
             TextField(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(start = 30.dp, end = 30.dp),
                 value = state.password,
                 label = {
                     Text(
-                        text = "Contraseña"
+                        text = "Password",
+                        color = Color.White
                     )
                 },
                 onValueChange = { password ->
                     viewModel.setPassword(password)
-                }
+                },
+                colors = TextFieldDefaults.colors(
+                    focusedContainerColor = BackgroundTextFieldColor,
+                    unfocusedContainerColor = BackgroundTextFieldColor,
+                    disabledContainerColor = BackgroundTextFieldColor,
+                    focusedTextColor = Color.White,
+                    unfocusedTextColor = Color.White,
+                    cursorColor = Color.White,
+                    focusedIndicatorColor = BackgroundTextFieldColor,
+                    unfocusedIndicatorColor = BackgroundTextFieldColor,
+                )
             )
+            Spacer(modifier = Modifier.height(32.dp))
             Button(
                 onClick = {
                     viewModel.navigateToMap()
-                }
+                },
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = LoginButtonColor,
+                ),
+                shape = RoundedCornerShape(10.dp)
             ) {
                 Text(
-                    text = "Iniciar Sesión"
+                    text = "Iniciar Sesión",
+                    color = Color.White,
+                    fontWeight = FontWeight.ExtraBold
                 )
             }
+            Spacer(modifier = Modifier.weight(0.48f))
             Text(
-                text = "Todos los derechos reservados 2025"
+                text = "Todos los derechos reservados 2025",
+                color = Color.White
             )
         }
     }
--- app/src/main/java/com/example/inovimap/ui/theme/Color.kt
+++ app/src/main/java/com/example/inovimap/ui/theme/Color.kt
@@ -9,3 +9,9 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+val BackgroundColor = Color(0xFF292C35)
+
+val BackgroundTextFieldColor = Color(0xFF12161F)
+
+val LoginButtonColor = Color(0xFF00B2B2)
